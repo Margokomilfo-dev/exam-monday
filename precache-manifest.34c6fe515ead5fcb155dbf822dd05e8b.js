d413f8618cebfee7fb093c1cba1b0d11db0e8deb
Margokomilfo-dev <margokomilfo@mail.ru>
2020-11-08 11:56:11 +0100

    Updates

---
+++ precache-manifest.34c6fe515ead5fcb155dbf822dd05e8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671f54c242c5347143bbf6540b1388fc",
+    "url": "/exam-monday/index.html"
+  },
+  {
+    "revision": "8ca6dce245709c8241c2",
+    "url": "/exam-monday/static/css/main.285361e6.chunk.css"
+  },
+  {
+    "revision": "b8c009e6a80b48396d17",
+    "url": "/exam-monday/static/js/2.78e90498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exam-monday/static/js/2.78e90498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca6dce245709c8241c2",
+    "url": "/exam-monday/static/js/main.1121a83e.chunk.js"
+  },
+  {
+    "revision": "999a7bd4409fa281c39f",
+    "url": "/exam-monday/static/js/runtime-main.4308ace7.js"
+  }
+]);
